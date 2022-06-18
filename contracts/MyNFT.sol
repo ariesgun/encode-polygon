eb99ebec482f694f851a6c2cc3b4b08f8c794d92
ariesgun <aries.thio@hotmail.com>
2022-06-18 13:12:31 +0200

    Add NFT contract

---
+++ contracts/MyNFT.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.8.0;
+//SPDX-License-Identifier: MIT
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721, Ownable {
+
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  constructor() public ERC721("MyNFT", "MNFT") {
+    _setBaseURI("https://ipfs.io/ipfs/");
+  }
+
+  function mintItem(address to, string memory tokenURI)
+      public
+      onlyOwner
+      returns (uint256)
+  {
+
+      uint256 id = _tokenIds.current();
+      _safeMint(to, id);
+      _setTokenURI(id, tokenURI);
+
+      _tokenIds.increment();
+
+      return id;
+  }
+
+}
+
+
